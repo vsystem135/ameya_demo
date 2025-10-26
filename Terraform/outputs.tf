5f61d659ac1b62506925db0998fc20a4c2684d54
v3 system <vsystem135@gmail.com>
2025-10-26 20:03:41 +0530

    chore:upate file

--- Terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  value       = aws_s3_bucket.this.id
-  description = "Name of the S3 bucket"
-}
-
-output "bucket_arn" {
-  value       = aws_s3_bucket.this.arn
-  description = "ARN of the S3 bucket"
-}
