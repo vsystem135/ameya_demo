08496d1bb731ffdb8294227779db9b7657af583f
v3 system <vsystem135@gmail.com>
2025-10-28 23:28:56 +0530

    required_version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,14 @@
 terraform {
+  required_version = ">= 1.9.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-/*
-  backend "s3" {
-    # Optional if you store Terraform state remotely
-  }
-*/
 }
+
 
 provider "aws" {
   region = "ap-south-1"
